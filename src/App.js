3948c107133a7b02b80ff1c9d94bf3236a08b63a
ngp00003 <156865918+ngp00003@users.noreply.github.com>
2024-06-13 13:52:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
 	<h1>CS 230L</h1>
-      <h2>Section - 702</h2>
-      <p>WVU ID: 800382869</p> {/* Replace XXXXXXX with your WVU ID */}
-      <p>Hi I am Nicholas Price</p> {/* Replace X with your name */}
+     	<h2>Section - 702</h2>
+      	<p>WVU ID: 800382869</p>
+      	<p>Hi I am Nicholas Price</p>
      </div>
   );
 }
