19d62074a611302c3fe3c90406415af1d6f5a108
Nick Price <ngp00003@mix.wvu.edu>
2024-06-13 13:44:42 -0400

    Updated App.js with personal details

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+	<h1>CS 230L</h1>
+      <h2>Section - 702</h2>
+      <p>WVU ID: 800382869</p> {/* Replace XXXXXXX with your WVU ID */}
+      <p>Hi I am Nicholas Price</p> {/* Replace X with your name */}
+     </div>
+  );
+}
+
+export default App;
