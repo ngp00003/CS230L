666ac553bc3df3842226de2103d44635bb4a07a2
Nick Price <ngp00003@mix.wvu.edu>
2024-06-20 02:17:44 -0400

    Navbar Updaate 3

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,13 +8,13 @@ const Navbar = () => {
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
         <div className="navbar-nav">
           <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
           <a className="nav-item nav-link" href="#">About</a>
           <a className="nav-item nav-link" href="#">Contact</a>
         </div>
-        <form className="form-inline my-2 my-lg-0">
+        <form className="form-inline">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
