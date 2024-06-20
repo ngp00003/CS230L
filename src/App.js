c0b5337c81b58a3a3c024a7c07cfc3ef83053725
Nick Price <ngp00003@mix.wvu.edu>
2024-06-20 01:18:04 -0400

    AWS 2 Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-	<h1>CS 230L</h1>
+      <Navbar />
+      <h1>CS 230L</h1>
       <h2>Section - 702</h2>
-      <p>WVU ID: 800382869</p> {/* Replace XXXXXXX with your WVU ID */}
-      <p>Hi I am Nicholas Price</p> {/* Replace X with your name */}
-     </div>
+      <p>WVU ID: 800382869</p>
+      <p>Hi I am Nicholas Price</p>
+      <Card />
+    </div>
   );
 }
 
