41a7215a2c87179a586dd2995833a4feeb15e9f4
Nick Price <ngp00003@mix.wvu.edu>
2024-06-20 02:08:46 -0400

    Navbar Update AWS 2

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,10 +14,10 @@ const Navbar = () => {
             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">About</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
       </div>
